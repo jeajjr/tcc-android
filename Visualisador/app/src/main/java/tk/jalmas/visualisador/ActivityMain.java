19c12f6d88fe6ee4e33af1abb5a98e4e7df11d50
jeajjr <jeajjr@gmail.com>
2015-09-15 18:22:48 -0300

    Project initialized

---
+++ Visualisador/app/src/main/java/tk/jalmas/visualisador/ActivityMain.java
@@ -0,0 +1,16 @@
+package tk.jalmas.visualisador;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jeajjr on 15/09/2015.
+ */
+public class ActivityMain extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
