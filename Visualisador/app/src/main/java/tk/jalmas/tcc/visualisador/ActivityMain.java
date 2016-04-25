31fdd7743552b31f59a7a9fe8a0ba03f396b157e
jeajjr <jeajjr@gmail.com>
2016-04-25 20:38:02 -0300

    Updating project to Android Studio 2.0

--- Visualisador/app/src/main/java/tk/jalmas/tcc/visualisador/ActivityMain.java
+++ Visualisador/app/src/main/java/tk/jalmas/tcc/visualisador/ActivityMain.java
@@ -1,16 +1,13 @@
-package tk.jalmas.visualisador;
+package tk.jalmas.tcc.visualisador;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/**
- * Created by jeajjr on 15/09/2015.
- */
-public class ActivityMain extends Activity {
+public class ActivityMain extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         getFragmentManager().beginTransaction()
--- Visualisador/app/src/main/java/tk/jalmas/tcc/visualisador/FragmentGraph.java
+++ Visualisador/app/src/main/java/tk/jalmas/tcc/visualisador/FragmentGraph.java
@@ -1,17 +1,19 @@
-package tk.jalmas.visualisador;
+package tk.jalmas.tcc.visualisador;
 
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import tk.jalmas.tcc.visualisador.R;
 
 
 /**
@@ -22,20 +24,41 @@ public class FragmentGraph extends Fragment {
 
     private GraphView graph;
 
+    final int nSamples = 400;
+    final int offset = 200;
+    final DataPoint[] dataPoints = new DataPoint[nSamples];
+    static int periodical = 0;
+
+    private Handler refreshHandler = null;
+    Runnable refresherRunnable = new Runnable() {
+        @Override
+        public void run() {
+            updateGraph();
+            refreshHandler.postDelayed(refresherRunnable, 1500);
+            Log.d(TAG, "last update tag updated");
+        }
+    };
     public FragmentGraph() {
         // Required empty public constructor
     }
 
+    private void updateGraph(){
+        DataPoint[] dataPoints = new DataPoint[nSamples];
 
+        for (int i = -offset; i < nSamples - offset; i++)
+            dataPoints[i + offset] = new DataPoint(i * 2 * Math.PI/nSamples, Math.sin(i * 2 * Math.PI/nSamples + periodical * Math.PI/2) );
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+        graph.removeAllSeries();
+        graph.addSeries(series);
+        periodical++;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_graph, container, false);
 
-        int nSamples = 400;
-        int offset = 200;
-        DataPoint[] dataPoints = new DataPoint[nSamples];
+
 
         for (int i = -offset; i < nSamples - offset; i++)
             dataPoints[i + offset] = new DataPoint(i * 2 * Math.PI/nSamples, Math.sin(i * 2 * Math.PI/nSamples));
@@ -43,13 +66,22 @@ public class FragmentGraph extends Fragment {
         graph = (GraphView) v.findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
         graph.addSeries(series);
+        graph.onDataChanged(false, false);
+
+        v.findViewById(R.id.toggle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "toggle button");
+
+            }
+        });
 
         v.findViewById(R.id.voltageScaleSelectorUp).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "voltageScaleSelectorUp");
 
-                graph.setScrollY(graph.getScaleY() / 2.0f);
+                //graph.setScrollY((int) (graph.getScaleY() / 2.0f));
             }
         });
 
@@ -57,11 +89,11 @@ public class FragmentGraph extends Fragment {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "voltageScaleSelectorDown");
-
-                graph.setScaleY(graph.getScaleY() * 2.0f);
             }
         });
 
+        refreshHandler = new Handler();
+        refresherRunnable.run();
         return v;
     }
 
